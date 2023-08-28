5a72f8d45166dfb04de0014d520ff04e3af1e037
RalphShindy <47220789+RalphShindy@users.noreply.github.com>
2023-08-28 14:04:00 +0000

    Updated

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById('counter').addEventListener("click", (e) => {
-        let count = parseInt(e.target.innerText);
-        count++;
-        e.target.innerText = count.toString();
+        e.target.innerText = (parseInt(e.target.innerText) + 1);
     });
 });
