ab91536be67b1bfaa2dec9ae1362d4beb6f1e0c8
Ralph Schindler <47220789+RalphShindy@users.noreply.github.com>
2023-08-28 22:36:45 +0200

    Updated

--- dist/assets/test-5852ecc1.js
+++
@@ -1,1 +0,0 @@
-import"./main-d47097eb.js";document.addEventListener("DOMContentLoaded",()=>{document.getElementById("counter").addEventListener("click",e=>{e.target.innerText=parseInt(e.target.innerText)+1})});
