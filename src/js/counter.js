16a7eceae4c467fcac0257e8ef84a0d269bebb36
RalphShindy <47220789+RalphShindy@users.noreply.github.com>
2023-08-30 13:32:05 +0000

    Updated

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,5 +1,30 @@
+import AOS from "aos";
+
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById('counter').addEventListener("click", (e) => {
-        e.target.innerText = (parseInt(e.target.innerText) + 1);
+    let counter = document.getElementById('counter');
+    let full = document.getElementById('counter_full');
+    let empty = document.getElementById('counter_empty');
+    full.style.display = parseInt(counter.value) === 10 ? 'block': 'none';
+    empty.style.display = parseInt(counter.value) === 0 ? 'block': 'none';
+
+    document.querySelectorAll('.change-counter').forEach(element => {
+        element.addEventListener("click", (e) => {
+
+            let newVal = (parseInt(counter.value) + parseInt(e.currentTarget.getAttribute("data-counter")));
+
+            counter.value = newVal < 0 ? 0 : newVal > 10 ? 10 : newVal;
+
+            if(parseInt(counter.value) === 10){
+                full.style.display = 'block';
+            }else{
+                full.style.display = 'none';
+            }
+
+            if(parseInt(counter.value) === 0){
+                empty.style.display = 'block';
+            }else{
+                empty.style.display = 'none';
+            }
+        });
     });
 });
--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,4 @@ import * as bootstrap from 'bootstrap';
 import AOS from "aos";
 import 'aos/dist/aos.css';
 
-AOS.init();
+AOS.init({duration: 1200,});
